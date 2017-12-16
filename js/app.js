c177cf50125488583cd97f28c7313d0740774e4d
Shannon <13708207+smckinney1@users.noreply.github.com>
2017-12-16 16:42:34 -0500

    Also displaying a list of cat nicknames using observable arrays.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,51 @@
 var ViewModel = function() {
 	//For now, putting the model in the ViewModel
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.google.com');
+	var self = this;
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+	self.clickCount = ko.observable(0);
+	self.name = ko.observable('Tabby');
+	self.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	self.imgAttribution = ko.observable('https://www.google.com');
+
+	self.nicknames = ko.observableArray([
+		{
+			nickname: 'Tabby'
+		},
+		{
+			nickname: 'Grumpypants'
+		},
+		{
+			nickname: 'Tiny'
+		},
+		{
+			nickname: 'Sleepy'
+		},
+		{
+			nickname: 'Happy'
+		}
+	]);
+
+	self.incrementCounter = function() {
+		self.clickCount(self.clickCount() + 1);
 	};
+
+	self.level = ko.computed(function() {
+		switch (true) {
+			case self.clickCount() < 10:
+				return 'Kitten';
+			case self.clickCount() < 20:
+				return 'Adult';
+			case self.clickCount() < 30:
+				return 'Old';
+		};
+		/*if (self.clickCount() < 10) {
+			return 'Kitten';
+		} else if (self.clickCount() < 20) {
+			return 'Adult';
+		} else {
+			return 'Old';
+		}*/
+	});
 }
 
 ko.applyBindings(new ViewModel());
