c2b4d6f940d4ab625da5bab26bf60ccc2a92b96a
Shannon <13708207+smckinney1@users.noreply.github.com>
2017-12-17 12:50:51 -0500

    Moved cat logic into its own function.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,11 @@
-var ViewModel = function() {
-	//For now, putting the model in the ViewModel
+var Cat = function () {
 	var self = this;
-
 	self.clickCount = ko.observable(0);
 	self.name = ko.observable('Tabby');
 	self.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	self.imgAttribution = ko.observable('https://www.google.com');
 
-	self.nicknames = ko.observableArray([
-		{
-			nickname: 'Tabby'
-		},
-		{
-			nickname: 'Grumpypants'
-		},
-		{
-			nickname: 'Tiny'
-		},
-		{
-			nickname: 'Sleepy'
-		},
-		{
-			nickname: 'Happy'
-		}
-	]);
-
-	self.incrementCounter = function() {
-		self.clickCount(self.clickCount() + 1);
-	};
+	self.nicknames = ko.observableArray(['Tabby', 'Grumpypants', 'Tiny', 'Sleepy', 'Happy']);
 
 	self.level = ko.computed(function() {
 		switch (true) {
@@ -38,14 +16,18 @@ var ViewModel = function() {
 			case self.clickCount() < 30:
 				return 'Old';
 		};
-		/*if (self.clickCount() < 10) {
-			return 'Kitten';
-		} else if (self.clickCount() < 20) {
-			return 'Adult';
-		} else {
-			return 'Old';
-		}*/
 	});
+}
+
+
+var ViewModel = function() {
+	var self = this;
+
+	self.currentCat = ko.observable(new Cat());
+
+	self.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
 }
 
 ko.applyBindings(new ViewModel());
